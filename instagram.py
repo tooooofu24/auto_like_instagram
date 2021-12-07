17e6fd7015ccd85c11c8743dea61eecf49831468
toya <toya24xxx@i.softbank.jp>
2021-12-07 22:48:29 +0900

    エラー処理の追加とインスタの制限に対応

--- instagram.py
+++ instagram.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from const import URL
 from const import PASSWORD
 from const import USER_ID
+import sys
 
 
 def login(driver):
@@ -14,12 +15,18 @@ def login(driver):
     print(driver.current_url)
 
     time.sleep(1)
-    WebDriverWait(driver, 15).until(
-        EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[3]/button")))
+    check = False
+    while check == False:
+        try:
+            login_btn = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, "button[type='submit']")))
+            check = True
+        except Exception:
+            print('ログインできません。システム終了します')
+            driver.quit()
+            sys.exit()
     driver.find_element(By.NAME, "username").send_keys(USER_ID)
     driver.find_element(By.NAME, "password").send_keys(PASSWORD)
-    login_btn = driver.find_element(
-        By.XPATH, '/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[3]/button')
 
     actions = ActionChains(driver)
     actions.move_to_element(login_btn)
@@ -54,7 +61,9 @@ def like_urls(driver, urls, count) -> int:
         if liked == True:
             print('いいね！しました')
             count += 1
+            # いいねした場合は30秒間隔を開ける
+            time.sleep(30)
         else:
             print('既にいいね！しています')
-        time.sleep(1)
+            time.sleep(2)
     return count
